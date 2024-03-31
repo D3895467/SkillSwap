5f42dbfe00677ccf21e77bba70f3e86748afb4ee
D3895467 <165485301+D3895467@users.noreply.github.com>
2024-03-31 02:09:13 +0100

    Initial commit

---
+++ app/src/main/java/uk/ac/tees/mad/d3895467/data/UserData.kt
@@ -0,0 +1,8 @@
+package uk.ac.tees.mad.d3895467.data
+
+data class UserData(
+    val name: String = "",
+    val email: String = "",
+    val password: String = "",
+    val image: String = ""
+)
---
+++ app/src/main/java/uk/ac/tees/mad/d3895467/screen/HomeScreen.kt
@@ -0,0 +1,7 @@
+package uk.ac.tees.mad.d3895467.screen
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+}
