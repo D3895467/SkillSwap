cd41c2768ec4c9c3ccf638974be966f6563d55e6
D3895467 <165485301+D3895467@users.noreply.github.com>
2024-04-27 12:32:15 +0100

    commit 5

--- app/src/main/java/uk/ac/tees/mad/d3895467/screen/SearchScreen.kt
+++ app/src/main/java/uk/ac/tees/mad/d3895467/screen/SearchScreen.kt
@@ -39,5 +39,188 @@ import uk.ac.tees.mad.d3895467.ui.theme.primaryLight
 
 @Composable
 fun SearchScreen(userId: String, navController: NavController) {
+    var text by rememberSaveable { mutableStateOf("") }
 
+    var skillListings by remember { mutableStateOf<List<SkillListing>>(emptyList()) }
+
+    // State to hold the filtered skill listings
+    var filteredSkillListings by remember { mutableStateOf<List<SkillListing>>(emptyList()) }
+
+    // Function to update filtered skill listings based on search query
+    fun updateFilteredSkillListings(query: String) {
+        filteredSkillListings = searchSkillListings(skillListings, query)
+    }
+
+    LaunchedEffect(Unit) {
+        skillListings = fetchSkillListings()
+        Log.d("search","$skillListings")
+        if (text.isNotBlank()) {
+            updateFilteredSkillListings(text)
+        }
+    }
+
+    Column (modifier = Modifier
+        .padding(18.dp)
+        .fillMaxSize()
+    ){
+
+        OutlinedTextField(
+            value = text,
+            onValueChange = { text = it },
+            label = {
+                Text("Search")
+            },
+            leadingIcon = {
+                Icon(
+                    Icons.Filled.Search,
+                    contentDescription = "Localized description",
+                    Modifier.clickable { updateFilteredSkillListings(text) },
+                    tint = primaryLight // Set tint color to primaryLight
+                )
+            },
+            modifier = Modifier
+                .padding(vertical = 1.dp)
+                .fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text
+            ),
+            shape = RoundedCornerShape(28.dp),
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedContainerColor = MaterialTheme.colorScheme.onPrimary,
+                focusedBorderColor = MaterialTheme.colorScheme.onBackground,
+                disabledBorderColor = MaterialTheme.colorScheme.onSurface,
+                disabledContainerColor = MaterialTheme.colorScheme.onSurface,
+                cursorColor = MaterialTheme.colorScheme.onPrimary,
+                focusedPlaceholderColor = MaterialTheme.colorScheme.onPrimary,
+            ),
+            singleLine = true,
+            trailingIcon = {
+                IconButton(
+                    onClick = {
+                        text = ""
+                        updateFilteredSkillListings(text)
+                    }
+                ) {
+                    Icon(
+                        Icons.Filled.Clear,
+                        contentDescription = "Localized description",
+                        tint = primaryLight // Set tint color to primaryLight
+                    )
+                }
+            }
+        )
+
+        // Display filtered skill listings
+        SkillRow(skillData = filteredSkillListings, navController  )
+    }
+}
+
+/*
+@Composable
+fun SearchScreen(userId: String, navController: NavController) {
+    var text by rememberSaveable { mutableStateOf("") }
+
+
+    var skillListings by remember { mutableStateOf<List<SkillListing>>(emptyList()) }
+
+    // Filter the skillListings based on searchQuery
+    */
+/*val filteredSkillListings = remember {
+        derivedStateOf {
+            searchSkillListings(skillListings, text)
+        }
+    }*//*
+
+
+    // State to hold the filtered skill listings
+    var filteredSkillListings by remember { mutableStateOf<List<SkillListing>>(emptyList()) }
+
+    // Function to update filtered skill listings based on search query
+    fun updateFilteredSkillListings(query: String) {
+        filteredSkillListings = searchSkillListings(skillListings, query)
+    }
+
+    LaunchedEffect(Unit) {
+        skillListings = fetchSkillListings()
+        Log.d("search","$skillListings")
+        if (text.isNotBlank()) {
+            updateFilteredSkillListings(text)
+        }
+    }
+
+    Column (modifier = Modifier
+        .padding(18.dp)
+        .fillMaxSize()
+        ){
+
+
+        OutlinedTextField(
+            value = text,
+            onValueChange = { text = it },
+            label = {
+                Text(
+                    "Search"
+                )
+            },
+            leadingIcon = {
+                Icon(
+                    Icons.Filled.Search,
+                    contentDescription = "Localized description",
+                    Modifier.clickable { updateFilteredSkillListings(text) }
+                )
+            },
+            modifier = Modifier
+                .clip(RoundedCornerShape(28.dp))
+                .padding( vertical = 1.dp)
+                .fillMaxWidth()
+            */
+/*.weight(weight = 0.09f, fill = true)*//*
+,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text
+            ),
+            shape = RoundedCornerShape(28.dp),
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedContainerColor = MaterialTheme.colorScheme.onPrimary,
+                focusedBorderColor = MaterialTheme.colorScheme.onBackground,
+                disabledBorderColor = MaterialTheme.colorScheme.onSurface,
+                disabledContainerColor = MaterialTheme.colorScheme.onSurface,
+                cursorColor = MaterialTheme.colorScheme.onPrimary,
+                focusedPlaceholderColor = MaterialTheme.colorScheme.onPrimary,
+            ),
+            singleLine = true,
+            trailingIcon = {
+                IconButton(
+                    onClick = {
+                        text = ""
+                        updateFilteredSkillListings(text)
+                    }
+                ) {
+                    Icon(
+                        Icons.Filled.Clear,
+                        contentDescription = "Localized description",
+                    )
+                }
+            }
+        )
+
+        // Display filtered skill listings
+        SkillRow(skillData = filteredSkillListings, navController  )
+    }
+}
+*/
+
+// Function to filter skill listings based on search query
+fun searchSkillListings(skillListings: List<SkillListing>, query: String): List<SkillListing> {
+    if (query.isBlank()) {
+        return skillListings // Return original list if query is blank
+    } else {
+        // Filter the skillListings based on the search query
+        return skillListings.filter { skill ->
+            skill.skillName.contains(query, ignoreCase = true) || skill.description.contains(
+                query,
+                ignoreCase = true
+            )
+        }
+    }
 }
